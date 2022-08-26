821fe8aea065575615e4ba3b2520f4038ace4e33
Fatbrother <kevin92422@gmail.com>
2022-08-26 19:16:58 +0800

    change return way

--- judgeLib/run.py
+++ judgeLib/run.py
@@ -2,39 +2,42 @@ import subprocess
 import psutil
 
 def run(file_dir: str, input: str, timeLimit: float, memoryLimit: int) -> str:
+    res = ''
+
     try:
         # run the program
         process = subprocess.Popen(
             [file_dir], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
-    except subprocess.CalledProcessError:
-        return 'CE'
+    except:
+        res = 'CE'
 
-    # calculate memory usage
-    memory_usage = psutil.Process(process.pid).memory_info().rss / 1024 / 1024
-    if memory_usage > memoryLimit:
-        process.terminate()
-        process.wait()
-        return 'MLE'
 
-    try:
-        output, error = process.communicate(
-            input.encode('utf-8', 'ignore'), timeout=timeLimit)
-    except subprocess.TimeoutExpired:
-        process.terminate()
-        process.wait()
-        return 'TLE'
-    except subprocess.CalledProcessError:
-        process.terminate()
-        process.wait()
-        return 'RE'
+    if res == '':
+        # calculate memory usage
+        memory_usage = psutil.Process(process.pid).memory_info().rss / (1024 ** 2)
+        if memory_usage > memoryLimit:
+            res = 'MLE'
 
-    # check if the program is something wrong
-    if error:
-        process.terminate()
-        process.wait()
-        return 'CE'
+    if res == '':
+        try:
+            output, error = process.communicate(
+                input.encode('utf-8', 'ignore'), timeout=timeLimit)
+        except subprocess.TimeoutExpired:
+            res = 'TLE'
+        except:
+            res = 'RE'
+
+
+    if res == '':
+        # check if the program is something wrong
+        if error:
+            res = 'CE'
 
     # close the process
     process.terminate()
     process.wait()
-    return output.decode('utf-8')
+
+    if res == '':
+        return output.decode('utf-8')
+    else:
+        return res
